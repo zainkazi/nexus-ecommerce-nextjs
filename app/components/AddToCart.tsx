de5a31282406c137f6ae21227c883013c287afca
Zain Kazi <zainkazi27@gmail.com>
2023-08-04 16:48:41 +0500

    fixed minor bugs

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCartStore } from "@/store";
+import { AddCartTypes } from "@/types/AddCartTypes";
 import { SearchParams } from "@/types/SearchParamTypes";
 import { useState } from "react";
 
@@ -10,17 +11,23 @@ const AddToCart = ({
   image,
   unit_amount,
   quantity,
-}: SearchParams) => {
+}: AddCartTypes) => {
   const { addProduct } = useCartStore();
+  const [adding, setAdding] = useState(false);
 
   return (
     <button
       onClick={() => {
         addProduct({ id, image, unit_amount, quantity, name });
+        setAdding(true);
+        setTimeout(() => {
+          setAdding(false);
+        }, 700);
       }}
+      disabled={adding}
       className="my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-700 disabled:opacity-25"
     >
-      Add to Cart
+      {adding ? "Adding to cart..." : "Add to Cart"}
     </button>
   );
 };
